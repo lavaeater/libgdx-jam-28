fef180de7b2424e756483f8bd5b5733a9727ff24
Tommie Nygren <tommie.nygren@gmail.com>
2024-03-04 10:16:11 +0100

    First commit, bro

---
+++ lwjgl3/src/main/kotlin/lava/core/lwjgl3/Lwjgl3Launcher.kt
@@ -0,0 +1,19 @@
+@file:JvmName("Lwjgl3Launcher")
+
+package lava.core.lwjgl3
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import lava.core.Jam28
+
+/** Launches the desktop (LWJGL3) application. */
+fun main() {
+    // This handles macOS support and helps on Windows.
+    if (StartupHelper.startNewJvmIfRequired())
+      return
+    Lwjgl3Application(Jam28(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("jam-28")
+        setWindowedMode(640, 480)
+        setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
+    })
+}
